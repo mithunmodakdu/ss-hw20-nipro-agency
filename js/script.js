26306d9e7da73d96b8e6be10e181645cfb0e83cf
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-09-27 20:24:42 +0600

    Footer copyright changed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+
 function openWork(evt, workName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
+    
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
 
     tablinks = document.getElementsByClassName("tablinks");
+    
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
